a87dc58b12bf176ecebf1086b318cb770077d18a
Goh Hong Xiang, Bryan <72204360+brygoh@users.noreply.github.com>
2022-09-21 00:11:26 +0800

    done

--- components/table.js
+++ components/table.js
@@ -6,22 +6,60 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
 
-export default function CustomTable({tableHead, tableBody}) {
+export default function CustomTable({ tableHead, tableBody }) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {tableHead.map((item, index) => {
-              return (
-                <TableCell key={index}>{item}</TableCell>
-              )
-            })}
-          </TableRow>
-        </TableHead>
-        <TableBody></TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper} sx={{ marginBottom: '32px' }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {tableHead.map((item, index) => {
+                return (
+                  <TableCell key={index}>{item}</TableCell>
+                )
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableBody ? tableBody.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => {
+              return <TableRow key={index}>
+                {Object.keys(item).map((item1, index1) => {
+                  if (item1 === 'team1' && parseInt(item['score1']) > parseInt(item['score2']) ||
+                    item1 === 'team2' && parseInt(item['score1']) < parseInt(item['score2'])) {
+                    return <TableCell sx={{ backgroundColor: 'green' }} key={index1}>{item[item1]}</TableCell>
+                  }
+                  if (item1 === 'team1' && parseInt(item['score1']) < parseInt(item['score2']) ||
+                    item1 === 'team2' && parseInt(item['score1']) > parseInt(item['score2'])) {
+                    return <TableCell sx={{ backgroundColor: 'red' }} key={index1}>{item[item1]}</TableCell>
+                  }
+                  if (parseInt(item['score1']) === parseInt(item['score2']) && (item1 === 'team1' || item1 === 'team2')) {
+                    return <TableCell sx={{ backgroundColor: 'yellow' }} key={index1}>{item[item1]}</TableCell>
+                  }
+                  return <TableCell key={index1}>{item[item1]}</TableCell>
+                })}
+
+              </TableRow>
+            }) : null}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={tableBody ? tableBody.length : 0}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={(_, page) => setPage(page)}
+          onRowsPerPageChange={(e) => {
+            setRowsPerPage(parseInt(e.target.value, 10));
+            setPage(0);
+          }}
+        />
+      </TableContainer>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -19,20 +19,130 @@ const tableHead2 = [
   'Team 2 Score',
 ]
 
+var groupDict = {}
+var leader1 = []
+var leader2 = []
+
 export default function Home() {
   const [register, setRegister] = useState()
+  const [rData, setRData] = useState()
   const [score, setScore] = useState()
+  const [sData, setSData] = useState()
 
   const checkRegexRegister = (value) => {
-    const expression = /[a-zA-Z]{0,255}\s[0-9][0-9]\/[0-9][0-9]\s[a-zA-Z]{0,255}$/
+    const expression = /^(?:[a-zA-Z]{1,255}\s[0-9][0-9]\/[0-9][0-9]\s[0-9]{1,255}\n?)+$/
     if (value.match(expression)) return true
     return false
   }
 
   const checkRegexScore = (value) => {
-    const expression = /[a-zA-Z]{0,255}\s[a-zA-Z]{0,255}\s[0-9]{0,255}\s[0-9]{0,255}$/
+    const expression = /^(?:[a-zA-Z]{1,255}\s[a-zA-Z]{1,255}\s[0-9]{1,255}\s[0-9]{1,255}\n?)+$/
     if (value.match(expression)) return true
     return false
+  }
+
+  function saveRegister(value) {
+    if(checkRegexRegister(value)) {
+      const arr = []
+      const dataArr = value.split('\n')
+      groupDict = {}
+      dataArr.map(item => {
+        if (item && item !== '') {
+          const temp = item.split(' ')
+          if (!groupDict[temp[0]]) {
+            groupDict[temp[0]] = parseInt(temp[2])
+            arr.push({
+              teamName: temp[0],
+              date: temp[1],
+              groupNo: temp[2],
+            })
+          }
+        }
+      })
+      setRData(arr)
+    } else {
+      alert('Wrongly formatted input')
+    }
+  }
+
+  const saveScore = async (value) => {
+    if(checkRegexScore(value) && rData) {
+      const arr = []
+      const dataArr = value.split('\n')
+      leader1 = []
+      leader2 = []
+      dataArr.map(async item => {
+        if (item && item !== '') {
+          const temp = item.split(' ')
+          if (temp[0] !== temp[1]) {
+            arr.push({
+              team1: temp[0],
+              team2: temp[1],
+              score1: temp[2],
+              score2: temp[3],
+            })
+          }
+          if (temp[2] > temp[3]) {
+            await handleLeaderboard(temp[0], 1);
+          }
+          if (temp[3] > temp [2]) {
+            await handleLeaderboard(temp[1], 1);
+          }
+          if (temp[2] === temp[3]) {
+            await handleLeaderboard(temp[0], 0.5);
+            await handleLeaderboard(temp[1], 0.5);
+          }
+        }
+      })
+      setSData(arr)
+    } else {
+      alert('Wrongly formatted input')
+    }
+  }
+
+  const handleLeaderboard = (team, score) => {
+    if (groupDict[team] === 1) {
+      const temp = leader1
+      var check = false;
+      temp.map((item, index) => {
+        if (item['team'] === team) {
+          check = true
+          leader1[index]['score'] += score
+        }
+      })
+      if (!check) {
+        leader1.push({team: team, score: parseInt(score)})
+        check = false
+      }
+    }
+    if (groupDict[team] === 2) {
+      const temp = leader2
+      var check = false;
+      temp.map((item, index) => {
+        if (item['team'] === team) {
+          check = true
+          leader2[index]['score'] += score
+        }
+      })
+      if (!check) {
+        leader2.push({team: team, score: parseInt(score)})
+        check = false
+      }
+    }
+    leader1.sort((a, b) => { return (b.score - a.score) })
+    leader2.sort((a, b) => { return (b.score - a.score) })
+  }
+
+  const spanTitle = (value) => {
+    return (
+      <span style={{fontSize: "20px", fontWeight: 600, width: '100%'}}>{value}</span>
+    )
+  }
+
+  const spanSubTitle = (value) => {
+    return (
+      <span style={{fontSize: "12px", width: '100%'}}>{value}</span>
+    )
   }
 
   return (
@@ -48,7 +158,14 @@ export default function Home() {
         }} 
         color="primary"
       >
-        <span className='index-appbar-title'>
+        <span className='index-appbar-title' 
+          style={{
+            fontSize: "24px", 
+            fontWeight: 600, 
+            display: 'flex', 
+            alignItems: 'center'
+          }}
+        >
           <SportsSoccer style={{ fontSize: '40px', marginRight: '8px' }} />
           We Are Champions
         </span>
@@ -59,31 +176,61 @@ export default function Home() {
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          height: `calc(100vh - 64px)`,
+          height: `100%`,
           minWidth: '320px',
           padding: '12px 24px',
           boxSizing: 'border-box',
           flexDirection: 'column',
           position: 'relative',
+          overflowY: 'scroll'
         }}
         maxWidth="lg"
       >
-        <TextField
-          sx={{
-            width: '100%',
-          }}
-          onChange={(e) => {setRegister(e.target.value)}}
-        />
-        <Button>Submit</Button>
-        <CustomTable tableHead={tableHead1}/>
-        <TextField
-          sx={{
-            width: '100%',
-          }}
-          onChange={(e) => {setScore(e.target.value)}}
-        />
-        <Button>Submit</Button>
-        <CustomTable tableHead={tableHead2}/>
+        {spanTitle("Register here:")}
+        {spanSubTitle("eg. firstTeam 17/05 2")}
+        <div style={{width: '100%', marginTop: '8px', marginBottom: '16px'}}>
+          <TextField
+            sx={{
+              width: `calc(100% - 72px)`,
+            }}
+            onChange={(e) => {setRegister(e.target.value)}}
+            multiline={true}
+          />
+          <Button 
+            sx={{width: '72px', height: '56px'}}
+            onClick={() => saveRegister(register)}
+          >Save</Button>
+        </div>
+        <CustomTable tableHead={tableHead1} tableBody={rData} />
+        {spanTitle("Register here:")}
+        {spanSubTitle("eg. firstTeam secondTeam 0 3")}
+        <div style={{width: '100%', marginTop: '8px', marginBottom: '16px'}}>
+          <TextField
+            sx={{
+              width: `calc(100% - 72px)`,
+            }}
+            onChange={(e) => {setScore(e.target.value)}}
+            multiline={true}
+          />
+          <Button 
+            sx={{width: '72px', height: '56px'}}
+            onClick={() => saveScore(score)}
+          >Save</Button>
+        </div>
+        <CustomTable tableHead={tableHead2} tableBody={sData} />
+        {spanTitle("Leaderboard for group 1:")}
+        {leader1.map((item, index) => {
+          return <span key={index}>{index+1}. {item.team}</span>
+        })}
+        {spanTitle("Leaderboard for group 2:")}
+        {leader2.map((item, index) => {
+          return <span key={index}>{index+1}. {item.team}</span>
+        })}
+        <Button 
+          variant="contained"
+          sx={{position: 'absolute', top: 8, right: 8}}
+          onClick={() => {setRData(); setSData(); groupDict = {}; leader1 = []; leader2 = []}}
+        >Clear All</Button>
       </Container>
     </>
   );
